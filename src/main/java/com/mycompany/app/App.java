32bf3776970408448d45c222f366ed3ce3bb67c7
Israel Rivas González <israelrivasgonzalez@gmail.com>
2021-03-17 21:44:01 +0000

    Add initial Jenkinsfile

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -3,9 +3,7 @@ package com.mycompany.app;
 /**
  * Hello world!
  */
-public class App
-{
-
+public class App {
     private final String message = "Hello World!";
 
     public App() {}
@@ -14,8 +12,7 @@ public class App
         System.out.println(new App().getMessage());
     }
 
-    private final String getMessage() {
+    private String getMessage() {
         return message;
     }
-
 }
